f79658b40dfd35042a604eb59c790116d9c9fcca
nasusfen <ravshannedonat@gmail.com>
2024-09-05 12:59:26 +0300

    Spawner fix

---
+++ Assets/Sources/CubeRainQuestV2/CubeSpawner.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CubeRainV2
+{
+	public class CubeSpawner : Spawner<RainyCube>
+	{
+		
+	}
+}
--- Assets/Sources/CubeRainQuestV2/Spawner.cs
+++ Assets/Sources/CubeRainQuestV2/Spawner.cs
@@ -4,21 +4,21 @@ using Random = UnityEngine.Random;
 
 namespace CubeRainV2
 {
-	public class Spawner : MonoBehaviour
+	public class Spawner<T> : MonoBehaviour where T: MonoBehaviour
 	{
-	[SerializeField] private MonoBehaviour _prefab;
+	[SerializeField] private T _prefab;
 	[SerializeField] private float _delay = 3f;
 	[SerializeField] private float _randomSpreadX = 1.1f;
 	[SerializeField] private float _randomSpreadZ = 1.1f;
 	[SerializeField] private int _startAmount = 1;
 	[SerializeField] private bool _isAutoSpawn = false;
 
-	private Pool<MonoBehaviour> _pool;
+	private Pool<T> _pool;
 	private WaitForSeconds _waitSpawn;
 
 	private void Awake()
 	{
-		_pool = new Pool<MonoBehaviour>(_prefab, transform, transform, _startAmount);
+		_pool = new Pool<T>(_prefab, transform, transform, _startAmount);
 		_waitSpawn = new WaitForSeconds(_delay);
 	}
 
@@ -35,7 +35,7 @@ namespace CubeRainV2
 
 	public void SpawnAt(Vector3 position)
 	{
-		MonoBehaviour spawnedObject = _pool.Peek();
+		T spawnedObject = _pool.Peek();
 		spawnedObject.transform.position = position;
 	}
 
@@ -58,7 +58,7 @@ namespace CubeRainV2
 			transform.position.y,
 			spawnPositionZ);
 
-		MonoBehaviour spawnedObject = _pool.Peek();
+		T spawnedObject = _pool.Peek();
 		spawnedObject.transform.position = spawnPosition;
 	}
 	}
