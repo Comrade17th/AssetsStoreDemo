61dbb16ba4bcfb09479ccd6d69fbc1b8017e867e
nasusfen <ravshannedonat@gmail.com>
2024-09-05 12:25:16 +0300

    some

--- Assets/Sources/CubeRainQuestV2/Spawner.cs
+++ Assets/Sources/CubeRainQuestV2/Spawner.cs
@@ -4,65 +4,62 @@ using Random = UnityEngine.Random;
 
 namespace CubeRainV2
 {
-	public class Spawner<T> : MonoBehaviour where T : MonoBehaviour
+	public class Spawner : MonoBehaviour
 	{
-		[SerializeField] private T _prefab;
-		[SerializeField] private float _delay = 3f;
-		[SerializeField] private float _randomSpreadX = 1.1f;
-		[SerializeField] private float _randomSpreadZ = 1.1f;
-		[SerializeField] private int _startAmount = 1;
-		[SerializeField] private bool _isAutoSpawn = false;
-    
-		private Pool<T> _pool;
-		private WaitForSeconds _waitSpawn;
+	[SerializeField] private MonoBehaviour _prefab;
+	[SerializeField] private float _delay = 3f;
+	[SerializeField] private float _randomSpreadX = 1.1f;
+	[SerializeField] private float _randomSpreadZ = 1.1f;
+	[SerializeField] private int _startAmount = 1;
+	[SerializeField] private bool _isAutoSpawn = false;
 
-		private void Awake()
-		{
-			_pool = new Pool<T>(_prefab, transform, transform, _startAmount);
-			_waitSpawn = new WaitForSeconds(_delay);
-		}
+	private Pool<MonoBehaviour> _pool;
+	private WaitForSeconds _waitSpawn;
 
-		private void Start()
-		{
-			if(_isAutoSpawn)
-				StartCoroutine(RandomSpawning());
-		}
+	private void Awake()
+	{
+		_pool = new Pool<MonoBehaviour>(_prefab, transform, transform, _startAmount);
+		_waitSpawn = new WaitForSeconds(_delay);
+	}
 
-		public void Reset()
-		{
-			_pool.Reset();
-		}
+	private void Start()
+	{
+		if (_isAutoSpawn)
+			StartCoroutine(RandomSpawning());
+	}
 
-		public void SpawnAt(Vector3 position)
-		{
-			T spawnedObject = _pool.Peek();
-			spawnedObject.transform.position = position;
-		}
+	public void Reset()
+	{
+		_pool.Reset();
+	}
 
-		private IEnumerator RandomSpawning()
-		{
-			while (enabled)
-			{
-				SpawnAtRandom();
-				yield return _waitSpawn;
-			}
-		}
+	public void SpawnAt(Vector3 position)
+	{
+		MonoBehaviour spawnedObject = _pool.Peek();
+		spawnedObject.transform.position = position;
+	}
 
-		private void SpawnAtRandom()
+	private IEnumerator RandomSpawning()
+	{
+		while (enabled)
 		{
-			float spawnPositionX = Random.Range(
-				transform.position.x - _randomSpreadX,
-				transform.position.x + _randomSpreadX);
-			float spawnPositionZ = Random.Range(
-				transform.position.z - _randomSpreadZ,
-				transform.position.z + _randomSpreadZ);
-			Vector3 spawnPosition = new Vector3(
-				spawnPositionX,
-				transform.position.y,
-				spawnPositionZ);
-      
-			T spawnedObject = _pool.Peek();
-			spawnedObject.transform.position = spawnPosition;
+			SpawnAtRandom();
+			yield return _waitSpawn;
 		}
+	}
+
+	private void SpawnAtRandom()
+	{
+		float spawnPositionX = Random.Range(transform.position.x - _randomSpreadX,
+			transform.position.x + _randomSpreadX);
+		float spawnPositionZ = Random.Range(transform.position.z - _randomSpreadZ,
+			transform.position.z + _randomSpreadZ);
+		Vector3 spawnPosition = new Vector3(spawnPositionX,
+			transform.position.y,
+			spawnPositionZ);
+
+		MonoBehaviour spawnedObject = _pool.Peek();
+		spawnedObject.transform.position = spawnPosition;
+	}
 	}
 }
